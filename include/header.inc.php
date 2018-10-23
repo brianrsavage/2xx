d6e9eede854f27c6aa81145efa4542f5fcf3b077
brianrsavage <bsavage@racc.edu>
2018-10-22 22:06:33 -0400

    added constant variables and updated sitename variables with constant

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+      // Pulls data information and configuration used through out the application.
+      require_once 'config.inc.php';
+
      // Pulls data elements used through out the entier website
      require_once 'content.data.php';
 
@@ -11,8 +14,6 @@
           variables in PHP and passing them into the HTML via Global
           JavaScript variables the same names used for PHP
      */
-
-     $siteName = 'inside out | 2xx';
 
 ?>
 
@@ -32,7 +33,7 @@
                for the browser, search engines, etc.
           */
           ?>
-          <title><?php echo $siteName; ?></title>
+          <title><?php echo SITENAME; ?></title>
           <?php
           /*
                used to display information about the
@@ -43,7 +44,7 @@
           <meta name="description" content="inside out is a class project we use to learn html."/>
           <link rel="stylesheet" type="text/css" href="assets/css/style.css">
           <script>
-               var siteName = '<?php echo $siteName; ?>';
+               var siteName = '<?php echo SITENAME; ?>';
           </script>
      </head>
      <?php
@@ -51,7 +52,7 @@
      ?>
      <body>
           <header>
-               <h1><a href="index.php"><?php echo $siteName; ?></a></h1>
+               <h1><a href="index.php"><?php echo SITENAME; ?></a></h1>
                <h2>continuously falling forward in to the light...</h2>
                <nav>
 			     <?php echo menuBuilder($content['pages']); ?>
