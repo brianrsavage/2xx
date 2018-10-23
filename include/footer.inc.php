158cc53c49656df032d959b8dca88313eff396c3
brianrsavage <bsavage@racc.edu>
2018-10-22 22:13:56 -0400

    added copyright and versioning

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -3,6 +3,7 @@
                <h4>this is the footer</h4>
                <p>we place redundant information here</p>
                <div id="footerTagLine">I am 7 inches left of normal</div>
+               <div class="copyright">iop project - Version <?php echo VERSION; ?> | &copy; 1971 - <?php echo date('Y'); ?> Brian Savage</div>
           </footer>
      </body>
 </html>
